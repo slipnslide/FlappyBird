0ae5238c749f60b5e09637ed08d1fb786a04652a
Slip <email": null>
2017-05-04 16:47:52 -0400

    fixed game restart

--- application.js
+++ application.js
@@ -91,7 +91,9 @@ function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
+            myObstacles = [];
             myGameArea.stop();
+            startGame();
         }
       }
     myGameArea.clear();
